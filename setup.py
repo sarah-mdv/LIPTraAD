82baa1f61a7277bb6d3348e87f9269d6aa7d7805
moyde <sarah.moydevitry@epfl.ch>
2021-03-01 22:01:08 +0100

    Add requirements and setup scripts

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='liptraad',
+      version='0.1',
+      description='Prototype learning',
+      author='Sarah Moy de Vitry',
+      license='MIT',
+      packages=['src'],
+      zip_safe=False)
